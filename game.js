be2f3c44e83fdce6be14790f9771cf1d1cfb7369
John Chun <johnchun10@gmail.com>
2021-07-15 10:51:18 -0400

    game update

--- game.js
+++ game.js
@@ -1,5 +1,8 @@
-var roundNum = 1;
+var roundNum = 0;
+var container = document.getElementById("gridContainer");
 var time = 60;
+var numRight = 0;
+var offBox;
 // function needs to work every three rounds 
 function blockNum(roundNum) {
     if (roundNum == 1) {return 4;}
@@ -24,29 +27,61 @@ document.getElementById("start-button").addEventListener("click", function () {
         timerbox.textContent = time
         time -= 1;
     }, 1000);
-        let color = randomColor();
-        let red = color.red;
-        let green = color.green;
-        let blue = color.blue;
-        let rgbColor = "rgb(" + red + ", " + green + ", " + blue + ")";
-        let offRed = red + 10;
-        let offGreen = green + 10;
-        let offBlue = blue + 10;
-        if (red > 245) {
-            offRed -= 20
-        }
-        if (blue > 245) {
-            offBlue -= 20
-        }
-        if (green > 245) {
-            offGreen -= 20
-        }
-        let offColor = "rgb(" + offRed + ", " + offGreen + ", " + offBlue + ")"
-        let boxes = document.getElementsByClassName("size2");
-        let offBox = boxes[Math.floor(Math.random() * boxes.length)];
-        for (let box of boxes) {
-            box.style.setProperty("background-color", rgbColor);
-        }
-        offBox.style.setProperty("background-color", offColor)
-        document.createElement("div")
+    document.getElementById("start-button").style.visibility = "hidden";
+    document.getElementById("start-instructions").style.visibility = "hidden";
+    createBoxes();
 })
+function createBoxes() {
+    roundNum++;
+    for (var r = 0; r < roundNum+1; r++) {
+        let row = document.createElement("div");
+        row.className = "row";
+        for(var c = 0; c < roundNum+1; c++) {
+            let col = document.createElement("div");
+            col.className = "col";
+            let colorDiv = document.createElement("div");
+            let boxStyle = "width: 100%; height: " + 700 / (roundNum+1) + "px; border-radius: 10px;";
+            colorDiv.style = boxStyle;
+            colorDiv.addEventListener("click", checker, false);
+            colorDiv.className = "colorDiv";
+            col.appendChild(colorDiv);
+            row.appendChild(col);
+        }
+        container.appendChild(row);
+    }
+    let color = randomColor();
+    let red = color.red;
+    let green = color.green;
+    let blue = color.blue;
+    let rgbColor = "rgb(" + red + ", " + green + ", " + blue + ")";
+    let offRed = red + 10;
+    let offGreen = green + 10;
+    let offBlue = blue + 10;
+    if (red > 245) {
+        offRed -= 20
+    }
+    if (blue > 245) {
+        offBlue -= 20
+    }
+    if (green > 245) {
+        offGreen -= 20
+    }
+    let offColor = "rgb(" + offRed + ", " + offGreen + ", " + offBlue + ")"
+    let boxes = document.getElementsByClassName("colorDiv");
+    offBox = boxes[Math.floor(Math.random() * boxes.length)];
+    for (let box of boxes) {
+        box.style.setProperty("background-color", rgbColor);
+    }
+    offBox.style.setProperty("background-color", offColor);
+}
+
+function checker() {
+    if (this == offBox) {
+        numRight++;
+        let rows = container.getElementsByClassName("row");
+        for (var r = rows.length-1; r >= 0; r--) {
+            container.removeChild(rows[r]);
+        }
+        createBoxes();
+    } 
+}
